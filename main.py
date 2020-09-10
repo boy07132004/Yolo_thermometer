338a0f45fe23188dbd775198d02a43b3d50783de
Zhe ming Wu <37643421+boy07132004@users.noreply.github.com>
2020-09-10 13:13:34 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,43 @@
 import subprocess
 import time
-pid = 0
+
+pid = None
 proc = None
+
 def run():
     global pid
     global proc
-    proc = subprocess.Popen("python ml6a01_api.py",)
-    pid = proc.pid
-run()
-while 1:
-    ps = subprocess.run(f"powershell Get-Process -id {pid}",shell=True,capture_output=True)
-    print(ps.stdout.split()[17])
-    if int(ps.stdout.split()[17])>1000:
-        proc.kill()
+    try:
+        while True:
+            # Check camera is online now.
+            try:
+                cam_server = "192.168.0.200"
+                # Ping 1 time in Windows OS.
+                check = subprocess.check_output(f"ping -n 1 {cam_server}") 
+                break
+            except:
+                print(time.ctime(),"Can't reach camera server")
+                time.sleep(6)
+        proc = subprocess.Popen(r"python D:\Thermometer\ML6A01\ml6a01_api.py")
+        pid = proc.pid
+        print(pid)
+    except:
+        print(time.ctime(),"Can't reach camera server")
+        time.sleep(6)
         run()
-    time.sleep(5)
+        
+#run()
+count=0
+
+while 1:
+    try:
+        # Process kill and restart when python crash.
+        ps = subprocess.check_output(f"powershell Get-Process -id {pid}",shell=True)
+        if count%100==0:print(time.ctime(),ps.split()[17])
+        if int(ps.split()[17])>1000:
+            proc.kill()
+            run()
+        time.sleep(6)
+        count+=1
+    except:
+        run()
--- ml6a01_api.py
+++ ml6a01_api.py
@@ -1,20 +1,24 @@
 from ctypes import *
-import random
 import os
 import cv2.cv2 as cv2
 import time
 import darknet
 import argparse
-from threading import Thread, enumerate
+import pyodbc
+from threading import Thread
 from queue import Queue,Empty
 from collections import Counter
-
+import logging
+FORMAT = '%(asctime)s %(message)s'
+logging.basicConfig(level=logging.DEBUG,filename='myLog.log', filemode='a', format=FORMAT)
+import os
+PID = os.getpid()
 def parser():
     parser = argparse.ArgumentParser(description="YOLO Object Detection")
     parser.add_argument("--weights", default="yolov4.weights",
                         help="yolo weights path")
 
-    parser.add_argument("--input", type=str, default="http://192.168.0.200:8081",
+    parser.add_argument("--input", type=str, default="http://10.96.32.29:8081",
                         help="URL")
 
     parser.add_argument("--config_file", default="yolov4.cfg",
@@ -40,42 +44,40 @@ def video_capture(darknet_image_queue):
     global cap
     global text
     while True:
-        while cap is None:
-            text = "Camera disconnect."
-            cap = get_video_capture(input_path)
-            time.sleep(10)
-        while cap.isOpened():
-            if text == "Camera disconnect.":text="請刷卡量測"
-            for _ in range(10):ret, frame = cap.read()
-            frame = cv2.rotate(frame,cv2.ROTATE_90_CLOCKWISE)
-            if not ret:break
-            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            frame_resized = cv2.resize(frame_rgb, (width, height),
-                                       interpolation=cv2.INTER_LINEAR)
-            darknet.copy_image_from_bytes(darknet_image, frame_resized.tobytes())
-            darknet_image_queue.put(darknet_image)
-        try:cap.release()
-        except:pass
+        try:
+            while cap.isOpened():
+                if text == "Camera disconnect.":text="請刷卡量測"
+                for _ in range(5):ret, frame = cap.read()
+                frame = cv2.rotate(frame,cv2.ROTATE_90_CLOCKWISE)
+                if not ret:break
+                frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                frame_resized = cv2.resize(frame_rgb, (width, height),
+                                           interpolation=cv2.INTER_LINEAR)
+                darknet.copy_image_from_bytes(darknet_image, frame_resized.tobytes())
+                darknet_image_queue.put(darknet_image)
+            logging.debug("video_capture cap is not opened.")
+            cap.release()
+        except:logging.debug("video_capture except.")
         cap = get_video_capture(input_path)
 infer_list = []
 flag = False
 def inference(darknet_image_queue):
+    prev_time = time.time()
     global infer_list
     while True:
-        while cap is None:ans=-99
-        while cap.isOpened():
-            darknet_image = darknet_image_queue.get()
-            prev_time = time.time()
-            detections = darknet.detect_image(network, class_names, darknet_image, thresh=args.thresh)
-            fps = int(1/(time.time() - prev_time))
-            ans = (darknet.print_detections(detections, True))
-            print(f"FPS: {fps} temp: {ans}")
-            if flag:infer_list.append(ans)
-            
-        try:cap.release()
-        except:time.sleep(10)
-
-
+        try:
+            while cap.isOpened():
+                darknet_image = darknet_image_queue.get()
+                detections = darknet.detect_image(network, class_names, darknet_image, thresh=args.thresh)
+                ans = (darknet.print_detections(detections, True))
+                print(f"temp: {ans}")
+                if flag:infer_list.append(ans)
+            logging.debug("Inference cap is not opened.")
+            cap.release()
+        except:
+            logging.debug("Inference except.")
+            time.sleep(5)
+   
 # Set IPCAM connection timeout
 class VideoCaptureDaemon(Thread):
     def __init__(self, video, result_queue):
@@ -85,6 +87,7 @@ class VideoCaptureDaemon(Thread):
         self.result_queue = result_queue
     def run(self):
         self.result_queue.put(cv2.VideoCapture(self.video))
+
 def get_video_capture(video, timeout=10):
     try:
         res_queue = Queue()
@@ -92,7 +95,9 @@ def get_video_capture(video, timeout=10):
         return res_queue.get(block=True, timeout=timeout)
     except Empty:
         print('cv2.VideoCapture: could not grab input ({}). Timeout occurred after {:.2f}s'.format(video, timeout))
-        return None
+        print("exit")
+        os.system(f"TASKKILL /PID {PID} /F")
+        print("exit_Done")
      
 from flask import Flask,render_template,request
 app = Flask(__name__)
@@ -114,20 +119,29 @@ def submit():
         if text != "Camera disconnect.":
             text = "請重新量測"
             print('RECV POST')
-            hid = request.values.get('hid')
             time_now = time.time()
+            hid = str(request.values.get('hid'))
+            infer_list.clear()
+            try:
+                cur.execute(f"select WORKID,NAME FROM HR_COPY WHERE HIDCARD='{hid}'")
+                work_ID,name = cur.fetchall()[0]
+            except:
+                work_ID = ""
+                name = hid
             flag = True
             while time.time()-time_now<12 and flag:
                 while time.time()-time_now<1.5:infer_list.clear()
-                while time.time()-time_now<4:pass
+                while time.time()-time_now<2:pass
                 counter = Counter(infer_list)
                 print(counter)
                 for temp,times in counter.items():
                     if temp>42 or temp<33:continue
                     if (times>2 and temp!=last_temp) or times>3:
-                        text = f"Hi  {hid} 您的體溫是 {temp} °C" if temp<37.4 else "體溫過高，請重新量測"
+                        text = f"Hi  {name} 您的體溫是 {temp} °C" if temp<37.4 else "體溫過高，請重新量測"
                         flag = False
                         last_temp=temp
+                        cur2.execute(f"INSERT INTO AUO_TEMP2 values ('{work_ID}','{name}','{temp}','{time.strftime('%Y-%m-%d %H:%M:%S')}','[額]')")
+                        con2.commit()
                         break
     return render_template("index.html",text=text)
     
@@ -137,6 +151,9 @@ if __name__ == '__main__':
     darknet_image_queue = Queue(maxsize=1)
     args = parser()
     check_arguments_errors(args)
+    input_path = args.input
+    cap = get_video_capture(input_path)
+    
     network, class_names, class_colors = darknet.load_network(
             args.config_file,
             args.data_file,
@@ -148,12 +165,19 @@ if __name__ == '__main__':
     width = darknet.network_width(network)
     height = darknet.network_height(network)
     darknet_image = darknet.make_image(width, height, 3)
-    input_path = args.input
-    cap = get_video_capture(input_path)
-    while cap is None:
-        cap = get_video_capture(input_path)
-        print("Wait cam")
-        
+    
     Thread(target=video_capture, args=(darknet_image_queue,),daemon=True).start()
     Thread(target=inference, args=(darknet_image_queue,),daemon=True).start()
-    app.run(host='0.0.0.0',debug=False)
+    
+    # DATABASE Information
+    server = '' 
+    database = '' 
+    username = ''
+    password = '' 
+    
+    con2 = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+    cur = con2.cursor()
+    cur2 = con2.cursor()
+    app.run(host='0.0.0.0',port=5001,debug=False)
+    cur.close()
+    cur2.close()
